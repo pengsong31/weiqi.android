accacdfe84260ef0939acff8f5858b906360f362
pengsong31 <pengsong31@163.com>
2013-06-23 22:05:23 +0800

    add weiqi application constants

---
+++ src/com/weiqi/app/constant/WeiQiConstants.java
@@ -0,0 +1,15 @@
+package com.weiqi.app.constant;
+
+/**
+ * 
+ * 类WeiQiConstants.java的实现描述：TODO 类实现描述 
+ * @author bruce.pengs 2013-6-23 下午8:27:03
+ */
+public class WeiQiConstants {
+
+    public static final int HTTP_SUCCESS = 0;
+    
+    public static final int HTTP_FAILURE = 1;
+    
+    public static final String  BASE_URL = "http://192.168.0.107/weiqi";
+}
